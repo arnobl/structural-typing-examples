5cd57014a0d321a8589a3fbc57e3151e800eaa93
arnobl <blouin.arno@gmail.com>
2017-01-30 21:05:56 +0100

    add python

---
+++ src/dynamicTyping/python/README.md
@@ -0,0 +1,4 @@
+
+## Python
+
+Python has a strong and dynamic type system (Python considers variables' types at run time). So, duck typing is used.
---
+++ src/dynamicTyping/python/duck.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
+class Duck(metaclass=ABCMeta):
+  @abstractmethod
+  def dance(self): 
+    pass
+  @abstractmethod
+  def talk(self):
+    pass
+
+class Mallard(Duck):
+  def dance(self):
+    return " _/¯ "
+  def talk(self):
+    return "quack quack"
+
+class Wolf:
+  def eat(self, duck):
+    return " 😈 "
+  def dance(self):
+    return " ¯\\_()_/¯ "
+  def talk(self):
+    return "QUACK QUACK WHOO"
+
+class Forest:
+  def twoDucksAlone(self, d1, d2):
+      print(d1.dance())
+      print(d2.dance())
+      print(d1.talk())
+      print(d2.talk())
+
+wolf = Wolf()
+theDuck = wolf
+aDuck = Mallard()
+aForest = Forest()
+
+
+aForest.twoDucksAlone(aDuck, theDuck)
+print(wolf.eat(aDuck))
